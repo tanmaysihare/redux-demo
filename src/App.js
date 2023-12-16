f3d44aa18a10af700331eb739fc4e74cac688ac1
tanmaysihare <tanmaysihare@gmail.com>
2023-12-16 06:31:57 +0530

    makes some changes but same as task 15 before

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
 import {useSelector} from 'react-redux';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <>
     <Header/>
      {!auth && <Auth />}
+     {auth && <UserProfile/>}
     <Counter />
 
     </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import classes from './Header.module.css';
 import {authActions} from '../store/index';
-import {useDispatch} from 'react-redux';
+import {useDispatch , useSelector} from 'react-redux';
 
 const Header = () => {
   const dispatch = useDispatch();
+  const auth = useSelector((state)=> state.auth.isAuthenticated);
   const logoutHandler = ()=> {
     dispatch(authActions.logout());
   }
@@ -11,7 +12,7 @@ const Header = () => {
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
-        <ul>
+       {auth && (<ul>
           <li>
             <a href='/'>My Products</a>
           </li>
@@ -21,7 +22,7 @@ const Header = () => {
           <li>
             <button onClick={logoutHandler}>Logout</button>
           </li>
-        </ul>
+        </ul>)}
       </nav>
     </header>
   );
